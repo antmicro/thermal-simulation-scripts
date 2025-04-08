12ee4cc4c905ea932892d0c68481509fd1827dbe
Szymon Magnuszewski <smagnuszewski@internships.antmicro.com>
2025-04-08 10:33:02 +0200

    [#74718] Add parameters source to film calculator

--- src/preprocessing/calculate_coef.py
+++ src/preprocessing/calculate_coef.py
@@ -1,13 +1,14 @@
 import logging
 
-# Air properties in 20ºC
-fluid_thermal_conductivity = 0.026  # W/m*ºC
-dynamic_viscosity = 0.0000182  # Pa*s
-fluid_specific_heat = 1004.5  # J/kg*⁰C
-volumetric_expansion = 0.00343  # 1/⁰C
-density = 1.204  # kg/m³
+# Air properties in 20ºC due to https://calcdevice.com/natural-convection-of-vertical-surface-id128.html
+fluid_thermal_conductivity = 0.0259  # W/m*ºC
+dynamic_viscosity = 0.000018  # Pa*s
+fluid_specific_heat = 1005  # J/kg*⁰C
+volumetric_expansion = 0.00365  # 1/⁰C
+density = 1.205  # kg/m³
 gravity = 9.80665  # m/s^2
-# plane constants
+# Nusselt number coefficients for natural convection of
+# top horizontal plane due to https://volupe.com/support/empirical-correlations-for-convective-heat-transfer-coefficients/
 n = 0.25
 c = 0.54
 
@@ -20,25 +21,25 @@ def calculate_film_coefficient(
 
     if orientation == "vertical":
         orientation_multiplier: float = 1
+        # c = 0.68
     elif orientation == "horizontal_up":
         orientation_multiplier = 1.3
+        # c = 0.54
     elif orientation == "horizontal_down":
         orientation_multiplier = 0.7
+        # c = 0.27
+
+    # fmt: off
     grashof_number = (
-        gravity
-        * pow(length, 3)
-        * volumetric_expansion
-        * (temp_surface - temp_fluid)
+        gravity * pow(length, 3) * volumetric_expansion * (temp_surface - temp_fluid)
         / pow((dynamic_viscosity / density), 2)
     )
-    prandtl_number = (
-        dynamic_viscosity * fluid_specific_heat / fluid_thermal_conductivity
-    )
+    prandtl_number = dynamic_viscosity * fluid_specific_heat / fluid_thermal_conductivity
     rayleigh_number = grashof_number * prandtl_number
     nusselt_number = c * pow(rayleigh_number, n)
-    film_coefficient = (
-        orientation_multiplier * nusselt_number * fluid_thermal_conductivity / length
-    )
+    film_coefficient = orientation_multiplier * nusselt_number * fluid_thermal_conductivity / length
+    # fmt: on
+
     logging.debug(f"Grashof_number = {grashof_number}")
     logging.debug(f"Prandtl_number = {prandtl_number}")
     logging.debug(f"Rayleigh_number = {rayleigh_number}")
